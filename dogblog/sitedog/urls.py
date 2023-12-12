68fcb31c376c4ddf35643096d0bb0a4b1705a757
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2023-12-12 15:46:45 +0300

    update urls

--- dogblog/sitedog/urls.py
+++ dogblog/sitedog/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-    path('cats/<int:cats_id>/', views.categories),
-    path('cats/<slug:cats_slug>/', views.categories_by_slug),
+    path('addpage/', views.addpage, name='add_page'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login'),
+    path('post/<int:post_id>/', views.show_post, name='post'),
 ]
