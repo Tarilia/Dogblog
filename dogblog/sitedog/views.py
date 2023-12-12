e953f138cf8a3ddcfd2c617fcae54496f844491b
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2023-12-12 15:49:36 +0300

    update views

--- dogblog/sitedog/views.py
+++ dogblog/sitedog/views.py
@@ -2,28 +2,43 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
 
-menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+menu = [
+        {'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'},
+]
+
+data_db = [
+    {'id': 1, 'title': 'Кавказская овчарка', 'content': 'Описание породы', 'is_published': True},
+    {'id': 2, 'title': 'Немецкий дог', 'content': 'Описание породы', 'is_published': True},
+    {'id': 3, 'title': 'Ротвейлер', 'content': 'Описание породы', 'is_published': False},
+]
 
 
 def index(request):
     return render(request, 'sitedog/index.html', context={'title': 'Главная страница',
-                                                          'menu': menu, })
+                                                          'menu': menu, 'posts': data_db, })
 
 
 def about(request):
     return render(request, 'sitedog/about.html', context={'title': 'О сайте',})
 
 
-def categories(request):
-    return HttpResponse('<h1>Статьи по категориям</h1>')
+def show_post(request, post_id):
+    return HttpResponse(f'Отображение статьи id: {post_id}')
 
 
-def categories(request, cats_id):
-    return HttpResponse(f'<h1>Статьи по категориям</h1><p>id: {cats_id}</p>')
+def addpage(request):
+    return HttpResponse(f'Добавление статьи')
 
 
-def categories_by_slug(request, cats_slug):
-    return HttpResponse(f'<h1>Статьи по категориям</h1><p>slug: {cats_slug}</p>')
+def contact(request):
+    return HttpResponse(f'Обратная связь')
+
+
+def login(request):
+    return HttpResponse(f'Авторизация')
 
 
 def page_not_found(request, exception):
