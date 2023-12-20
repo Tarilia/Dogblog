9913cfa0690e903c2232ca80a7e8c4b72748c295
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2023-12-20 14:45:19 +0300

    add Count for cats and tags

--- dogblog/sitedog/templatetags/sitedog_tags.py
+++ dogblog/sitedog/templatetags/sitedog_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from dogblog.sitedog.models import Category, TagPost
+from django.db.models import Count
 
 
 register = template.Library()
@@ -7,10 +8,10 @@ register = template.Library()
 
 @register.inclusion_tag('sitedog/list_categories.html')
 def show_categories(cat_selected=0):
-    cats = Category.objects.all()
+    cats = Category.objects.annotate(total=Count("posts")).filter(total__gt=0)
     return {'cats': cats, 'cat_selected': cat_selected}
 
 
 @register.inclusion_tag('sitedog/list_tags.html')
 def show_all_tags():
-    return {'tags': TagPost.objects.all()}
+    return {'tags': TagPost.objects.annotate(total=Count("tags")).filter(total__gt=0)}
