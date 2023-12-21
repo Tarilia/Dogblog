889c27874f9435e01de89f99030cac2f4ad2c350
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2023-12-21 15:22:10 +0300

    add search and filter

--- dogblog/sitedog/admin.py
+++ dogblog/sitedog/admin.py
@@ -10,6 +10,8 @@ class SitedogAdmin(admin.ModelAdmin):
     list_editable = ('is_published', )
     list_per_page = 5
     actions = ['set_published', 'set_draft']
+    search_fields = ['title__startswith', 'cat__name']
+    list_filter = ['cat__name', 'is_published', 'tags']
 
     @admin.display(description="Краткое описание", ordering='content')
     def brief_info(self, sitedog: Sitedog):
