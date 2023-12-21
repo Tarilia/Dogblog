63370c7743eb18684b4c649491513e5fe64132d9
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2023-12-21 15:06:35 +0300

    add actions

--- dogblog/sitedog/admin.py
+++ dogblog/sitedog/admin.py
@@ -1,14 +1,29 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from .models import Sitedog, Category
 
 
 @admin.register(Sitedog)
-class WomenAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'time_create', 'is_published', 'cat')
-    list_display_links = ('id', 'title')
+class SitedogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'time_create', 'is_published', 'cat', 'brief_info')
+    list_display_links = ('title', )
     ordering = ['-time_create', 'title']
     list_editable = ('is_published', )
     list_per_page = 5
+    actions = ['set_published', 'set_draft']
+
+    @admin.display(description="Краткое описание", ordering='content')
+    def brief_info(self, sitedog: Sitedog):
+        return f"Описание {len(sitedog.content)} символов."
+
+    @admin.action(description="Опубликовать выбранные записи")
+    def set_published(self, request, queryset):
+        count = queryset.update(is_published=Sitedog.Status.PUBLISHED)
+        self.message_user(request, f"Изменено {count} записей.")
+
+    @admin.action(description="Снять с публикации выбранные записи")
+    def set_draft(self, request, queryset):
+        count = queryset.update(is_published=Sitedog.Status.DRAFT)
+        self.message_user(request, f"{count} записей сняты с публикации!", messages.WARNING)
 
 
 @admin.register(Category)
