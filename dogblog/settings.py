9a8a816708964a726071c6c4ad41013f74eb2317
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2023-12-22 19:22:05 +0300

    update css admin

--- dogblog/settings.py
+++ dogblog/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'dogblog',
-    'dogblog.sitedog',
+    'dogblog.sitedog.apps.SitedogConfig',
     'debug_toolbar',
 ]
 
@@ -59,7 +58,7 @@ ROOT_URLCONF = 'dogblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates', ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,7 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
