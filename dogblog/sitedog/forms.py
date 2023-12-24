0ba5cd80f6046f984815c7b78cd575fd0307b131
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2023-12-24 14:10:01 +0300

    add validators

--- dogblog/sitedog/forms.py
+++ dogblog/sitedog/forms.py
@@ -1,11 +1,27 @@
 from django import forms
 from .models import Category
+from django.core.exceptions import ValidationError
+from django.utils.deconstruct import deconstructible
+
+
+@deconstructible
+class RussianValidator:
+    ALLOWED_CHARS = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯабвгдеёжзийклмнопрстуфхцчшщбыъэюя0123456789- "
+    code = 'russian'
+
+    def __init__(self, message=None):
+        self.message = message if message else "Должны присутствовать только русские символы, дефис и пробел."
+
+    def __call__(self, value, *args, **kwargs):
+        if not (set(value) <= set(self.ALLOWED_CHARS)):
+            raise ValidationError(self.message, code=self.code)
 
 
 class AddPostForm(forms.Form):
     title = forms.CharField(max_length=255, min_length=5,
                             label="Заголовок",
-                            widget=forms.TextInput(attrs={'class': 'form-input'}))
+                            widget=forms.TextInput(attrs={'class': 'form-input'}),
+                            validators=[RussianValidator(), ])
     slug = forms.SlugField(max_length=255, label="URL")
     content = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 5}), required=False, label="Контент")
     is_published = forms.BooleanField(required=False, initial=True, label="Статус")
