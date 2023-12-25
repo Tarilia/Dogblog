6740723c5f70b76bcccf3ac0f110c23081e4b0f3
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2023-12-25 13:49:55 +0300

    add photo in admin

--- dogblog/sitedog/admin.py
+++ dogblog/sitedog/admin.py
@@ -1,16 +1,17 @@
 from django.contrib import admin, messages
 from .models import Sitedog, Category
+from django.utils.safestring import mark_safe
 
 
 @admin.register(Sitedog)
 class SitedogAdmin(admin.ModelAdmin):
-    fields = ['title', 'slug', 'content', 'cat', 'tags']
+    fields = ['title', 'slug', 'content', 'photo', 'post_photo', 'cat', 'tags']
     # exclude = ['tags', 'is_published']
-    # readonly_fields = ['slug']
+    readonly_fields = ['post_photo']
     prepopulated_fields = {"slug": ("title", )}
     # filter_horizontal = ['tags']
     filter_vertical = ['tags']
-    list_display = ('title', 'time_create', 'is_published', 'cat', 'brief_info')
+    list_display = ('title', 'post_photo', 'time_create', 'is_published', 'cat')
     list_display_links = ('title', )
     ordering = ['-time_create', 'title']
     list_editable = ('is_published', )
@@ -18,10 +19,13 @@ class SitedogAdmin(admin.ModelAdmin):
     actions = ['set_published', 'set_draft']
     search_fields = ['title__startswith', 'cat__name']
     list_filter = ['cat__name', 'is_published', 'tags']
+    save_on_top = True
 
-    @admin.display(description="Краткое описание", ordering='content')
-    def brief_info(self, sitedog: Sitedog):
-        return f"Описание {len(sitedog.content)} символов."
+    @admin.display(description="Изображение", ordering='content')
+    def post_photo(self, sitedog: Sitedog):
+        if sitedog.photo:
+            return mark_safe(f"<img src='{sitedog.photo.url}' width=50>")
+        return "Без фото"
 
     @admin.action(description="Опубликовать выбранные записи")
     def set_published(self, request, queryset):
