f66202fa0175f19d1ac29edf77d903b284fa6bb2
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2024-01-04 13:59:26 +0300

    add RegisterUser(CreateView)

--- users/forms.py
+++ users/forms.py
@@ -5,23 +5,23 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 class LoginUserForm(AuthenticationForm):
     username = forms.CharField(label="Логин",
-                               widget=forms.TextInput(attrs={'class': 'form-input'}))
+                    widget=forms.TextInput(attrs={'class': 'form-input'}))
     password = forms.CharField(label="Пароль",
-                               widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+                    widget=forms.PasswordInput(attrs={'class': 'form-input'}))
 
     class Meta:
         model = get_user_model()
         fields = ['username', 'password']
 
 
-class RegisterUserForm():
+class RegisterUserForm(UserCreationForm):
     username = forms.CharField(label="Логин", widget=forms.TextInput(attrs={'class': 'form-input'}))
-    password = forms.CharField(label="Пароль", widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    password1 = forms.CharField(label="Пароль", widget=forms.PasswordInput(attrs={'class': 'form-input'}))
     password2 = forms.CharField(label="Повтор пароля", widget=forms.PasswordInput(attrs={'class': 'form-input'}))
 
     class Meta:
         model = get_user_model()
-        fields = ['username', 'email', 'first_name', 'last_name', 'password', 'password2']
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
         labels = {
             'email': 'E-mail',
             'first_name': "Имя",
@@ -32,12 +32,6 @@ class RegisterUserForm():
             'first_name': forms.TextInput(attrs={'class': 'form-input'}),
             'last_name': forms.TextInput(attrs={'class': 'form-input'}),
         }
-
-    def clean_password2(self):
-        cd = self.cleaned_data
-        if cd['password'] != cd['password2']:
-            raise forms.ValidationError("Пароли не совпадают!")
-        return cd['password']
 
     def clean_email(self):
         email = self.cleaned_data['email']
