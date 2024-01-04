9e9afbf352ed9f18a89f4eba44c9c332f4fb0431
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2024-01-04 14:14:08 +0300

    add EmailAuthBackend

--- dogblog/settings.py
+++ dogblog/settings.py
@@ -136,3 +136,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index'
 LOGIN_URL = 'users:login'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'users.authentication.EmailAuthBackend',
+]
